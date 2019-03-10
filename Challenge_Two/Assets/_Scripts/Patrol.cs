d0a7212ae10606c9e391fcccb3148ce443c11653
rhiannadoesart <46513874+rhiannadoesart@users.noreply.github.com>
2019-03-10 12:34:02 -0400

    continued challege work

---
+++ Challenge_Two/Assets/_Scripts/Patrol.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float PingPong;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(Mathf.PingPong(Time.time, 3),2);
+    }
+}
--- Challenge_Two/Assets/_Scripts/PlayerController.cs
+++ Challenge_Two/Assets/_Scripts/PlayerController.cs
@@ -1,25 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+
     public float speed;
     public float jumpForce;
+    public Text  countText;
+    public Text winText;
+    public Text livesText;
+    public Text loseText;
 
 
     // Use this for initialization
 	void Start () {
 
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        loseText.text = "";
+        SetCountText();
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey("escape"))
             Application.Quit();
+
     }
 
     void FixedUpdate()
@@ -43,5 +57,40 @@ public class PlayerController : MonoBehaviour {
             }
 
         }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 4)
+        {
+            winText.text = "You Win!";
+        }
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives < 1)
+        {
+            loseText.text = "You Lose...";
+            KillPlayer();
+        }
+    }
+    void KillPlayer()
+    {
+        gameObject.CompareTag("Player");
+        gameObject.SetActive(false);
     }
 }
